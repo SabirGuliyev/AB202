bc32eace3c4f812cc2ccf0307d3560da61a721cb
SabirGuliyev <sabir.guliyev@code.edu.az>
2023-09-27 19:01:08 +0400

    Control structure added

---
+++ Conditional/ConsoleApp4/Program.cs
@@ -0,0 +1,92 @@
+﻿
+
+//byte age = 17;
+//string gender = "kishi";
+
+//bool result = 5 > 8;
+
+//if (age>17)
+//{
+//    Console.WriteLine("Qebul olduz");
+
+//}
+//else if (age == 17)
+//{
+//    Console.WriteLine("Staj kecin");
+//}
+//else if (age == 16)
+//{
+//    Console.WriteLine("Tedrise qoshul");
+//}
+//else
+//{
+//    Console.WriteLine("Ugursuz namized");
+//}
+
+
+
+
+//byte choice = 3;
+
+
+//switch (choice)
+//{
+//    case 1:
+//        Console.WriteLine("az dili secildi");
+//        break;
+
+//    case 2 or 3 or 4:
+//        Console.WriteLine("diger diller secildi");
+//        break;
+
+//    default:
+//        Console.WriteLine("Bele secimimiz yoxdur");
+//        break;
+//}
+
+
+//string day = "monday";
+
+//switch (day)
+//{
+//    case "monday":
+//    case "tuesday":
+//        Console.WriteLine("ish gunu");
+//        break;
+//    case "sunday" or "saturday":
+//        Console.WriteLine("Istirahet");
+//        break;
+//    default:
+//        break;
+//}
+
+int example = 0;
+
+if (example > 0)
+{
+    Console.WriteLine("Musbet");
+}
+else if (example == 0)
+{
+    Console.WriteLine("Neytral");
+}
+else 
+{
+    Console.WriteLine("Menfi");
+}
+
+
+int discount = 0;
+
+
+
+if (discount!=0)
+{
+
+    Console.WriteLine(200 / discount);
+}
+
+
+
+
+
---
+++ LoopsAp202/LoopsAp202/Program.cs
@@ -0,0 +1,114 @@
+﻿//Console.WriteLine("Yashinizi daxil edin?");
+
+//int num = int.Parse(Console.ReadLine());
+
+
+
+
+
+//string answer = "";
+//do
+//{
+//    Console.WriteLine("Dovr devam etsinmi?");
+//    answer=Console.ReadLine();
+
+//} while (answer=="yes");
+
+
+
+//string name =Console.ReadLine();
+
+
+//Console.WriteLine(name);
+
+
+//string answer = "";
+//Console.WriteLine("Dovr devam etsinmi?");
+//answer = Console.ReadLine();
+//while (answer == "yes")
+//{
+//    Console.WriteLine("Dovr devam etsinmi?");
+//    answer = Console.ReadLine();
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//bool result = false;
+
+//while (!result)
+//{
+//    Console.WriteLine("Adinizi daxil edin");
+//    string name=Console.ReadLine();
+
+//    if (name=="Sabir")
+//    {
+//        result = true;
+//    }
+
+//}
+
+
+//Console.WriteLine("Dovr bitdi");
+
+
+//int i = 0;
+//while (i<10)
+//{
+//    Console.WriteLine(i);
+//    i++;
+//}
+
+//Task 1
+//for (int i = 1; i < 90; i++)
+//{
+//    if (i%2==0)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
+
+//int sum=0;
+//for (int i = 1; i < 100; i++)
+//{
+//    sum += i;
+//}
+
+//Console.WriteLine(sum);
+
+
+
+
+//for (int i = 1; i < 100; i++)
+//{
+//    if (i%5==0)
+//    {
+//        continue;
+//    }
+
+//    Console.WriteLine(i);
+//}
+
+
+int num = 45123;
+int count=0;
+
+while (num != 0)
+{
+    num /= 10;
+    count++;
+}
+
+Console.WriteLine(count);
+
+
