153ab54b904e564c5310c43f91a26707c2fb9177
SabirGuliyev <sabir.guliyev@code.edu.az>
2023-11-04 16:58:39 +0400

    ADO.NET added

---
+++ ADONETExample/ADONETExample/Program.cs
@@ -0,0 +1,72 @@
+﻿using ADONETExample.Models;
+using ADONETExample.Services;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ADONETExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            #region ADO intro
+            string conn = "server=MSI;database=ADONETAB202;trusted_connection=true;integrated security=true;";
+            //SqlConnection connection = new SqlConnection(conn);
+            //connection.Open();
+
+            //string cmd = "INSERT INTO Students VALUES('Test','Testov',18)";
+            //SqlCommand command = new SqlCommand(cmd, connection);
+
+            //int result = command.ExecuteNonQuery();
+            //connection.Close();
+
+            //if (result>0)
+            //{
+            //    Console.WriteLine("Command successfully completed");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Error occured");
+            //}
+
+            //string query = "SELECT * FROM Students";
+            //SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+
+            //DataTable table = new DataTable();
+            //adapter.Fill(table);
+
+            //connection.Close();
+
+
+            //foreach(DataRow item in table.Rows)
+            //{
+
+            //    Console.WriteLine(item["Id"]+" " + item["Name"]+" " + item["Surname"]+" " + item["Age"]);
+            //}
+
+
+            #endregion
+
+            StudentService service=new StudentService();
+
+           
+
+            Student student = new Student { Name="Azer",Surname="Alasgerli",Age=22};
+
+            //service.Add(student);
+
+            service.Delete(4);
+
+            List<Student> students = service.GetAll();
+
+            foreach (Student item in students)
+            {
+                Console.WriteLine($"{item.Id} {item.Name} {item.Surname} {item.Age}");
+            }
+
+
+        }
+    }
+}
---
+++ ADONETExample/ADONETExample/Services/StudentService.cs
@@ -0,0 +1,62 @@
+﻿using ADONETExample.Data;
+using ADONETExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADONETExample.Services
+{
+    internal class StudentService
+    {
+        private static Sql _database=new Sql();
+
+        public void Add(Student student)
+        {
+            string cmd = $"INSERT INTO Students VALUES('{student.Name}','{student.Surname}',{student.Age})";
+            int result= _database.ExecuteCommand(cmd);
+
+            if (result>0)
+            {
+                Console.WriteLine("Command successfully completed");
+            }
+            else
+            {
+                Console.WriteLine("Error occured");
+            }
+        }
+
+        public List<Student> GetAll()
+        {
+
+            string query = "SELECT * FROM Students";
+            DataTable table= _database.ExecuteQuery(query);
+
+            List<Student> students= new List<Student>();
+
+            foreach (DataRow row in table.Rows)
+            {
+                Student student = new Student
+                {
+                    Id =(int) row["id"],
+                    Name = row["name"].ToString(),
+                    Surname = row["surname"].ToString(),
+                    Age = Convert.ToInt32(row["age"])
+
+                };
+                students.Add(student);
+
+            }
+            return students;
+        }
+
+        public void Delete(int id)
+        {
+            string cmd = $"DELETE FROM Students WHERE Id={id}";
+
+            _database.ExecuteCommand(cmd);
+        }
+    }
+}
