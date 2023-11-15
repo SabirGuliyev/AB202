386d1a6b76d41b4f74ed57a11c4ee6bf5eccaa2c
SabirGuliyev <sabir.guliyev@code.edu.az>
2023-11-15 18:42:59 +0400

    Viewbag added

---
+++ VieBagExampleAB202/VieBagExampleAB202/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VieBagExampleAB202.Models;
+
+namespace VieBagExampleAB202.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            List<Student> students = new List<Student>();
+            students.Add(new Student { Id=1,Name="Fidan",Surname="Behbudova",Age=32});
+            students.Add(new Student { Id=2,Name="Nubar",Surname="Abudova",Age=19});
+            students.Add(new Student { Id=3,Name="Elbrus",Surname="Mirzayev",Age=19});
+            students.Add(new Student { Id=4,Name="Amin",Surname="Pashayev",Age=19});
+
+            //ViewBag.Students=students;
+
+            //ViewData["Students"]=students;
+
+            //TempData["Name"] = students;
+
+
+            return View(students);
+        }
+        public IActionResult Detail()
+        {
+            string name = TempData["Name"].ToString();
+            TempData.Keep("Name");
+
+
+            return Content(name);
+        }
+
+        public IActionResult About()
+        {
+            //string name = TempData["Name"].ToString();
+            //Console.WriteLine(name);
+
+            return View();
+
+            //string name = TempData["Name"].ToString();
+            //TempData.Keep("Name");
+
+
+            //return Content(name);
+        }
+        
+    }
+}
