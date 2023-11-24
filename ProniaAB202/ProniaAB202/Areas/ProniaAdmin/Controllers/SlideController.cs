ca80e50d9df3bec5dd078ac605d42ab9f71fe872
SabirGuliyev <sabir.guliyev@code.edu.az>
2023-11-24 14:16:26 +0400

    FileUpload finished

--- ProniaAB202/ProniaAB202/Areas/ProniaAdmin/Controllers/SlideController.cs
+++ ProniaAB202/ProniaAB202/Areas/ProniaAdmin/Controllers/SlideController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using ProniaAB202.DAL;
 using ProniaAB202.Models;
+using ProniaAB202.Utilities.Extensions;
 
 namespace ProniaAB202.Areas.ProniaAdmin.Controllers
 {
@@ -9,9 +10,12 @@ namespace ProniaAB202.Areas.ProniaAdmin.Controllers
     public class SlideController : Controller
     {
         private readonly AppDbContext _context;
-       public SlideController(AppDbContext context)
+        private readonly IWebHostEnvironment _env;
+
+        public SlideController(AppDbContext context, IWebHostEnvironment env)
         {
             _context = context;
+            _env = env;
         }
 
         public async Task<IActionResult> Index()
@@ -26,36 +30,102 @@ namespace ProniaAB202.Areas.ProniaAdmin.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(Slide slide)
         {
+
             if (slide.Photo is null)
             {
-                ModelState.AddModelError("Photo","Shekil mutlew secilmelidir");
+                ModelState.AddModelError("Photo", "Shekil mutlew secilmelidir");
                 return View();
             }
 
-            if (!slide.Photo.ContentType.Contains("image/"))
+            if (!slide.Photo.ValidateType("image/"))
             {
                 ModelState.AddModelError("Photo", "File tipi uygun deyil");
                 return View();
             }
 
-            if (slide.Photo.Length>2*1024*1024)
+            if (!slide.Photo.ValidateSize(2*1024))
             {
                 ModelState.AddModelError("Photo", "File olcusu 2 mb den boyuk olmamalidir");
                 return View();
             }
 
-            FileStream file = new FileStream(@"C:\Users\sabir\Desktop\AB202\ProniaAB202\ProniaAB202\wwwroot\assets\images\slider\" + slide.Photo.FileName, FileMode.Create);
-            await slide.Photo.CopyToAsync(file);
 
-            slide.Image = slide.Photo.FileName;
-
-
-
-
-            //return Content(slide.Photo.FileName + " " + slide.Photo.ContentType + " " + slide.Photo.Length);
+            slide.Image =await slide.Photo.CreateFileAsync(_env.WebRootPath, "assets", "images", "slider");
+            
             await _context.Slides.AddAsync(slide);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Update(int id)
+        {
+            if (id <= 0) return BadRequest();
+
+            Slide existed = await _context.Slides.FirstOrDefaultAsync(s => s.Id == id);
+
+            if (existed is null) return NotFound();
+
+            return View(existed);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(int id,Slide slide)
+        {
+
+            Slide existed = await _context.Slides.FirstOrDefaultAsync(s => s.Id == id);
+            if (existed is null) return NotFound();
+           
+            if (!ModelState.IsValid)
+            {
+                return View(existed);
+            }
+
+            if (slide.Photo is not null)
+            {
+                if (!slide.Photo.ValidateType("image/"))
+                {
+                    ModelState.AddModelError("Photo", "File tipi uygun deyil");
+                    return View(existed);
+                }
+
+                if (!slide.Photo.ValidateSize(2 * 1024))
+                {
+                    ModelState.AddModelError("Photo", "File olcusu 2 mb den boyuk olmamalidir");
+                    return View(existed);
+                }
+                string fileName = await slide.Photo.CreateFileAsync(_env.WebRootPath, "assets", "images", "slider");
+                existed.Image.DeleteFile(_env.WebRootPath, "assets", "images", "slider");
+
+                existed.Image = fileName;
+            }
+
+            existed.Title=slide.Title;
+            existed.SubTitle = slide.SubTitle;
+            existed.Order = slide.Order;
+            existed.Description = slide.Description;
+            await _context.SaveChangesAsync();
+
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id <= 0) return BadRequest();
+
+            Slide existed=await _context.Slides.FirstOrDefaultAsync(s=>s.Id==id);
+
+            if (existed is null) return NotFound();
+
+            existed.Image.DeleteFile(_env.WebRootPath,"assets","images","slider");
+
+            _context.Slides.Remove(existed);
+
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+           
+           
         }
     }
 }
--- ProniaAB202/ProniaAB202/Models/Slide.cs
+++ ProniaAB202/ProniaAB202/Models/Slide.cs
@@ -8,9 +8,9 @@ namespace ProniaAB202.Models
         public string Title { get; set; }
         public string SubTitle { get; set; }
         public string Description { get; set; }
-        public string Image { get; set; }
+        public string? Image { get; set; }
         public int Order { get; set; }
         [NotMapped]
-        public IFormFile Photo { get; set; }
+        public IFormFile? Photo { get; set; }
     }
 }
